7671f728ac9559e503c1179e512dcdb900e4df9c
cherhy <ekxk1234@naver.com>
2023-10-02 21:32:42 +0900

    apply repository test

--- src/test/kotlin/dev/fastcampus/springmvc/SpringMvcApplicationTests.kt
+++ src/test/kotlin/dev/fastcampus/springmvc/SpringMvcApplicationTests.kt
@@ -1,13 +1,34 @@
 package dev.fastcampus.springmvc
 
+import dev.fastcampus.springmvc.model.Article
+import dev.fastcampus.springmvc.repository.ArticleRepository
+import mu.KotlinLogging
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class SpringMvcApplicationTests {
+private val logger = KotlinLogging.logger{}
 
+@SpringBootTest
+class SpringMvcApplicationTests(
+    @Autowired private val articleRepository: ArticleRepository,
+) {
+    private val logger = KotlinLogging.logger{}
     @Test
     fun contextLoads() {
+        val prev = articleRepository.count()
+        articleRepository.save(
+            Article(
+                title = "title",
+                body = "body",
+                authorId = 1L,
+            )
+        ).let {logger.debug { it }}
+
+        val current = articleRepository.count()
+        logger.debug { "prev = $prev, current = $current" }
+        Assertions.assertThat(current).isEqualTo(prev + 1)
     }
 
 }
